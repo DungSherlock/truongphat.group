4fe864b44b5ad5faf28f6cce98327715ea608493
DungSherlock <dungsherlock@gmail.com>
2021-03-21 23:17:33 +0700

    update info home

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,14 +3,14 @@ let gtm = '1234'
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Landing Page Starter`,
-    description: `A slightly opinionated Gatsby Landing Page starter template. `,
-    author: `Jeremy Lynch`,
-    siteUrl: `https://gatsby-strapi-starter.netlify.com/`,
+    title: `Trường Phát Holdings`,
+    description: `Công ty cổ phần Trường Phát Holdings`,
+    author: `HueDung`,
+    siteUrl: `https://truongphat.group/`,
     phone: '1800123456',
     fax: '180012345',
-    address: '123 fake street',
-    email: 'contact@test.com'
+    address: 'Ninh Bình',
+    email: 'lienhe@truongphat.group'
 
   },
   plugins: [
